da93b08ac8abc878670957f5278a1cfe8fb76241
@slovacus <slovacus@gmail.com>
2020-12-21 20:52:47 -0500

    chore: implement load config (#3)

---
+++ config/base.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+export CHEAT_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export CHEAT_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
+export CHEAT_MESSAGE_YAY="Please install yay or use antibody bundle luismayta/zsh-yay branch:develop"
+export CHEAT_PACKAGE_NAME="cheat"
