a4127ff0dc4ab8eccaaf291ac7c71385a01f55e3
Luis Mayta <luis@hadenlabs.com>
2020-12-21 20:52:47 -0500

    chore: implement load config (#3)

---
+++ config/main.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function cheat::config::main::factory {
+    # shellcheck source=/dev/null
+    source ${ZSH_CHEAT_PATH}/config/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source ${ZSH_CHEAT_PATH}/config/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source ${ZSH_CHEAT_PATH}/config/linux.zsh
+      ;;
+    esac
+}
+
+cheat::config::main::factory
