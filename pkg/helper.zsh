e7f15a435ba7511906f175312fdc8a929ed6c6ab
Luis Mayta <luis@hadenlabs.com>
2021-01-31 19:31:34 -0500

    chore: implement alias edit cheat configuration (#14)

--- pkg/helper.zsh
+++ pkg/helper.zsh
@@ -1,2 +1,11 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+# editcheat edit settings for cheat
+function editcheat {
+    if [ -z "${EDITOR}" ]; then
+        message_warning "it's neccesary the var EDITOR"
+        return
+    fi
+    "${EDITOR}" "${CHEAT_FILE_SETTINGS}"
+}
