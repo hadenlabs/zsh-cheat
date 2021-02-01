1ab63038998cca7e37b47b77c0792da58cf18d1c
@slovacus <slovacus@gmail.com>
2021-01-31 19:55:15 -0500

    chore: implement sync for cheat configuration (#16)

--- pkg/base.zsh
+++ pkg/base.zsh
@@ -10,3 +10,7 @@ function cheat::post_install {
     message_info "Installing ${CHEAT_PACKAGE_NAME}"
     message_success "Installed ${CHEAT_PACKAGE_NAME}"
 }
+
+function cheat::sync {
+    rsync -avzh --progress "${ZSH_CHEAT_PATH}/conf/" "${CHEAT_CONFIG_PATH}/"
+}
