5bf762d1daccc57949af60d2413d2cb64624ef3e
@slovacus <slovacus@gmail.com>
2021-01-31 23:47:45 -0500

    refactor: change cheat config path to cheat path (#21)

--- config/base.zsh
+++ config/base.zsh
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 export CHEAT_USE_FZF=true
-export CHEAT_CONFIG_PATH="${HOME}"/.config/cheat
-export CHEAT_CHEATSHEET_PATH="${CHEAT_CONFIG_PATH}"/cheatsheets
-export CHEAT_FILE_SETTINGS="${CHEAT_CONFIG_PATH}"/conf.yml
+export CHEAT_PATH="${HOME}"/.config/cheat
+export CHEAT_CHEATSHEET_PATH="${CHEAT_PATH}"/cheatsheets
+export CHEAT_FILE_SETTINGS="${CHEAT_PATH}"/conf.yml
 export CHEAT_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 export CHEAT_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
 export CHEAT_MESSAGE_YAY="Please install yay or use antibody bundle luismayta/zsh-yay branch:develop"
