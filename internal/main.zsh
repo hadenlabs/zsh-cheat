07a02438e926ca1b0c5c6cfbcad9ab986ed766d1
Luis Mayta <luis@hadenlabs.com>
2021-03-12 15:42:21 -0500

    refactor: change validation exists program (#23)

--- internal/main.zsh
+++ internal/main.zsh
@@ -3,20 +3,20 @@
 
 function cheat::internal::main::factory {
     # shellcheck source=/dev/null
-    source ${ZSH_CHEAT_PATH}/internal/base.zsh
+    source "${ZSH_CHEAT_PATH}"/internal/base.zsh
     case "${OSTYPE}" in
     darwin*)
         # shellcheck source=/dev/null
-        source ${ZSH_CHEAT_PATH}/internal/osx.zsh
+        source "${ZSH_CHEAT_PATH}"/internal/osx.zsh
         ;;
     linux*)
         # shellcheck source=/dev/null
-        source ${ZSH_CHEAT_PATH}/internal/linux.zsh
+        source "${ZSH_CHEAT_PATH}"/internal/linux.zsh
       ;;
     esac
 }
 
 cheat::internal::main::factory
 
-if ! type -p fzf > /dev/null; then cheat::internal::fzf::install; fi
-if ! type -p cheat > /dev/null; then cheat::internal::cheat::install; fi
+if ! core::exists fzf; then cheat::internal::fzf::install; fi
+if ! core::exists cheat; then cheat::internal::cheat::install; fi
