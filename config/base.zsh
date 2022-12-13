9c6c5b92908e2a6fd143b1d1a8d8202b78fbabb8
Luis Mayta <luis@hadenlabs.com>
2022-12-12 21:05:27 -0500

    chore: implement install strategy cheat (#30)
    * chore: implement install strategy cheat (#30)
    
    * feat: implement tag version configuration (#30)

--- config/base.zsh
+++ config/base.zsh
@@ -5,10 +5,11 @@ export CHEAT_USE_FZF=true
 export CHEAT_PATH="${HOME}"/.config/cheat
 export CHEAT_CHEATSHEET_PATH="${CHEAT_PATH}"/cheatsheets
 export CHEAT_FILE_SETTINGS="${CHEAT_PATH}"/conf.yml
-export CHEAT_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew branch:develop"
-export CHEAT_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm branch:develop"
-export CHEAT_MESSAGE_YAY="Please install yay or use antibody bundle hadenlabs/zsh-yay branch:develop"
+export CHEAT_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew"
+export CHEAT_MESSAGE_RVM="Please install rvm or use antibody bundle hadenlabs/zsh-rvm"
+export CHEAT_MESSAGE_YAY="Please install yay or use antibody bundle hadenlabs/zsh-yay"
 export CHEAT_PACKAGE_NAME="cheat"
 export CHEAT_CHEATSHEETS=(
     hadenlabs/cheatsheet
 )
+export CHEAT_PACKAGE_GO="github.com/cheat/cheat/cmd/cheat@latest"
